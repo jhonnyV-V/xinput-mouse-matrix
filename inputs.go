14e8668e458deb149329055d3cc75829c8b77551
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-09-29 17:55:03 -0400

    basic ui

---
+++ inputs.go
@@ -0,0 +1,170 @@
+package main
+
+// A simple example demonstrating the use of multiple text input components
+// from the Bubbles component library.
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	inputHelpStyle      = blurredStyle
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
+type inputModel struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode cursor.Mode
+}
+
+func initialModel() inputModel {
+	m := inputModel{
+		inputs: make([]textinput.Model, 3),
+	}
+
+	var t textinput.Model
+	for i := range m.inputs {
+		t = textinput.New()
+		t.Cursor.Style = cursorStyle
+		t.CharLimit = 32
+
+		switch i {
+		case 0:
+			t.Placeholder = "X axis acceleration"
+			t.Focus()
+			t.PromptStyle = focusedStyle
+			t.TextStyle = focusedStyle
+			t.CharLimit = 6
+		case 1:
+			t.Placeholder = "Y axis acceleration"
+			t.CharLimit = 6
+		case 2:
+			t.Placeholder = "Acceleration"
+			t.CharLimit = 6
+		}
+
+		m.inputs[i] = t
+	}
+
+	return m
+}
+
+func (m inputModel) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m inputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+
+		// Change cursor mode
+		case "ctrl+r":
+			m.cursorMode++
+			if m.cursorMode > cursor.CursorHide {
+				m.cursorMode = cursor.CursorBlink
+			}
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := range m.inputs {
+				cmds[i] = m.inputs[i].Cursor.SetMode(m.cursorMode)
+			}
+			return m, tea.Batch(cmds...)
+
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				return m, tea.Quit
+			}
+
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				if i == m.focusIndex {
+					// Set focused state
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+
+			return m, tea.Batch(cmds...)
+		}
+	}
+
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *inputModel) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m inputModel) View() string {
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	b.WriteString(inputHelpStyle.Render("cursor mode is "))
+	b.WriteString(cursorModeHelpStyle.Render(m.cursorMode.String()))
+	b.WriteString(inputHelpStyle.Render(" (ctrl+r to change style)"))
+
+	return b.String()
+}
---
+++ main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func listItems() ([]list.Item, error) {
+	items := []list.Item{}
+	var buff bytes.Buffer
+	cmd := exec.Command("xinput", "list", "--name-only")
+	cmd.Stdout = &buff
+	err := cmd.Run()
+
+	rawOutput := buff.String()
+
+	devices := strings.Split(rawOutput, "\n")
+
+	for _, device := range devices {
+		items = append(items, item(device))
+	}
+
+	return items, err
+}
+
+func getCurrentValue(deviceName string) (float32, float32, float32, error) {
+	var x, y, acceleration float32
+	var values []string
+
+	cmd := exec.Command("xinput", "--list-props", deviceName)
+	var buff bytes.Buffer
+	cmd.Stdout = &buff
+	err := cmd.Run()
+
+	b := buff.String()
+	props := strings.Split(b, "\n")[1:]
+
+	for _, prop := range props {
+		clean := strings.TrimSpace(prop)
+		if !strings.Contains(clean, "Coordinate Transformation Matrix") {
+			continue
+		}
+
+		values = strings.Split(
+			strings.TrimSpace(strings.Split(clean, ":")[1]),
+			",",
+		)
+	}
+
+	temp, err := strconv.ParseFloat(
+		strings.TrimSpace(values[0]),
+		32,
+	)
+	if err != nil {
+		return x, y, acceleration, err
+	}
+	x = float32(temp)
+
+	temp, err = strconv.ParseFloat(
+		strings.TrimSpace(values[4]),
+		32,
+	)
+	if err != nil {
+		return x, y, acceleration, err
+	}
+	y = float32(temp)
+
+	temp, err = strconv.ParseFloat(
+		strings.TrimSpace(values[8]),
+		32,
+	)
+	if err != nil {
+		return x, y, acceleration, err
+	}
+	acceleration = float32(temp)
+
+	return x, y, acceleration, err
+}
+
+func setValue(x, y, acceleration float32, deviceName string) error {
+	cmd := exec.Command(
+		"xinput", "set-prop",
+		deviceName,
+		"Coordinate Transformation Matrix",
+		fmt.Sprintf("%f,", x), "0,", "0,",
+		"0,", fmt.Sprintf("%f,", y), "0,",
+		"0,", "0,", fmt.Sprintf("%f,", acceleration),
+	)
+	err := cmd.Run()
+	return err
+}
+
+func main() {
+	items, err := listItems()
+
+	if err != nil {
+		fmt.Printf("error %s\n", err)
+		return
+	}
+
+	const defaultWidth = 20
+
+	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+	l.Title = "What do you want for dinner?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
+	m := model{list: l}
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
+	x, y, acceleration, err := getCurrentValue(Choice)
+	if err != nil {
+		fmt.Printf("error getting current values %s\n", err)
+		return
+	}
+
+	fmt.Printf("x:%f y:%f acceleration:%f\n", x, y, acceleration)
+
+	if _, err := tea.NewProgram(initialModel()).Run(); err != nil {
+		fmt.Printf("could not start program: %s\n", err)
+		os.Exit(1)
+	}
+}
